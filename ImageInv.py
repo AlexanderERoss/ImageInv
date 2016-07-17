d28b2349b9d9822ed2db3cdd0f715c5d99eb0dfe
Alexander Ross <alexander.eric.ross@gmail.com>
2016-07-17 19:25:49 +1000

    Added script file

---
+++ ImageInv.py
@@ -0,0 +1,10 @@
+################################################################################
+#Image Inverter
+#Made by Alexander Ross
+#alexander.eric.ross@gmail.com
+########################################
+#This program is made to invert an image around a point, taking the boundary
+#and tranforming it to a central point and takes the central point and
+#stretches it to the boundary.
+################################################################################
+#
